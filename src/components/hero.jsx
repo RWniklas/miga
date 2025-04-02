3ffc45c4b5e1758e2494d7151348ccb8d6062849
RWniklas <[nicki.sissigmx.de]>
2025-04-02 22:49:53 +0200

    first commit

---
+++ src/components/hero.jsx
@@ -0,0 +1,30 @@
+export function Hero() {
+  
+    return (
+        <div class="row">
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/hero1.png" alt="Image 1"/>
+            </div>
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/hero2.png" alt="Image 2"/>
+            </div>
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/values_main.png" alt="Image 4"/>
+            </div>
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/archive.png" alt="Image 5"/>
+            </div>
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/studio.png" alt="Image 3"/>
+            </div>
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/hero3.png" alt="Image 6"/>
+            </div>
+            <div class="col-md-2 col-6 image-container">
+                <img src="images/homepage/hero4.png" alt="Image 7"/>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
