aa16981860b09a7188c3c9a130014c152b5911e6
RWniklas <[nicki.sissigmx.de]>
2025-04-05 01:17:28 +0200

    hero component

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,21 @@
-export function Hero() {
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import React from 'react';
+
+import Hero from './components/Hero/Hero.jsx';
+
+import './App.css'
+function App() {
+    const [count, setCount] = useState(0);
+    
   return (
-    <div className="row justify-content-between gx-3 px-3 py-4">
-      {/* Image 1 */}
-      <div className="col d-flex justify-content-center">
-        <img src="images/homepage/hero1.png" alt="Image 1" className="img-fluid" />
-      </div>
-
-      {/* Image 2 with offset */}
-      <div className="col d-flex justify-content-center" style={{ transform: 'translateY(170px)' }}>
-        <img src="images/homepage/hero2.png" alt="Image 2" className="img-fluid" />
-      </div>
-
-      {/* Image 3 - Value */}
-      <div className="col d-flex justify-content-center">
-        <div className="card border-0 text-center" style={{ width: '100%' }}>
-          <img src="images/homepage/values.png" alt="Value" className="card-img-top img-fluid" />
-          <div className="card-body p-2">
-            <p className="card-text small">01&nbsp;&nbsp;&nbsp;Value</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Image 4 - Archive */}
-      <div className="col d-flex justify-content-center">
-        <div className="card border-0 text-center" style={{ width: '100%' }}>
-          <img src="images/homepage/archive.png" alt="Archive" className="card-img-top img-fluid" />
-          <div className="card-body p-2">
-            <p className="card-text small">02&nbsp;&nbsp;&nbsp;Archive</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Image 5 - Studios */}
-      <div className="col d-flex justify-content-center">
-        <div className="card border-0 text-center" style={{ width: '100%' }}>
-          <img src="images/homepage/studio.png" alt="Studios" className="card-img-top img-fluid" />
-          <div className="card-body p-2">
-            <p className="card-text small">03&nbsp;&nbsp;&nbsp;Studios</p>
-          </div>
-        </div>
-      </div>
-
-      {/* Image 6 with offset */}
-      <div className="col d-flex justify-content-center" style={{ transform: 'translateY(-140px)' }}>
-        <img src="images/homepage/hero3.png" alt="Image 6" className="img-fluid" />
-      </div>
-
-      {/* Image 7 */}
-      <div className="col d-flex justify-content-center">
-        <img src="images/homepage/hero4.png" alt="Image 7" className="img-fluid" />
-      </div>
-    </div>
+    
+      <Hero/>
+      
   );
 }
 
-export default Hero;
+
+
+export default App;
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "./Hero.css"; // External styles
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+function Hero() {
+  return (
+    <div className="hero-container">
+      {/* Image 1 */}
+      <div className="image-box">
+        <img src="images/homepage/hero1.png" alt="Image 1" className="image" />
+      </div>
+
+      {/* Image 2 with offset */}
+      <div className="image-box" style={{ transform: "translateY(160px)" }}>
+        <i className="fa-solid fa-plus plus-icon"></i>
+        <img src="images/homepage/hero2.png" alt="Image 2" className="image" />
+      </div>
+
+      {/* Image 3 - Value */}
+      <div className="border-box">
+        <div className="image-box">
+          <img src="images/homepage/values.png" alt="Value" className="image" />
+          <div className="label"><b>01&nbsp;&nbsp;Value</b></div>
+        </div>
+      </div>
+
+      {/* Image 4 - Archive */}
+      <div className="border-box">
+        <div className="image-box">
+          <img src="images/homepage/archive.png" alt="Archive" className="image" />
+          <div className="label"><b>02&nbsp;&nbsp;Archive</b></div>
+        </div>
+      </div>
+
+      {/* Image 5 - Studios */}
+      <div className="border-box">
+        <div className="image-box">
+          <img src="images/homepage/studio.png" alt="Studios" className="image" />
+          <div className="label">03&nbsp;&nbsp;Studios</div>
+        </div>
+      </div>
+
+      {/* Image 6 with off-set*/}
+      <div className="image-box">
+        <i className="fa-solid fa-plus plus-icon-2"></i>
+        <img src="images/homepage/hero3.png" alt="Image 6" className="image" style={{ transform: "translateY(-170px)" }} />
+      </div>
+
+      {/* Image 7 */}
+      <div className="image-box">
+        <img src="images/homepage/hero4.png" alt="Image 7" className="image" />
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
