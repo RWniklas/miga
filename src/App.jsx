0add8e0718e01c08a7337d597b5015b8ec97305f
RWniklas <[nicki.sissigmx.de]>
2025-04-04 19:43:02 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import { useState } from 'react'
-import  Hero from './components/Hero';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+export function Hero() {
   return (
-    <div className="App">
+    <div className="row justify-content-between gx-3 px-3 py-4">
+      {/* Image 1 */}
+      <div className="col d-flex justify-content-center">
+        <img src="images/homepage/hero1.png" alt="Image 1" className="img-fluid" />
+      </div>
 
-      
-      <Hero />
+      {/* Image 2 with offset */}
+      <div className="col d-flex justify-content-center" style={{ transform: 'translateY(170px)' }}>
+        <img src="images/homepage/hero2.png" alt="Image 2" className="img-fluid" />
+      </div>
 
+      {/* Image 3 - Value */}
+      <div className="col d-flex justify-content-center">
+        <div className="card border-0 text-center" style={{ width: '100%' }}>
+          <img src="images/homepage/values.png" alt="Value" className="card-img-top img-fluid" />
+          <div className="card-body p-2">
+            <p className="card-text small">01&nbsp;&nbsp;&nbsp;Value</p>
+          </div>
+        </div>
+      </div>
 
+      {/* Image 4 - Archive */}
+      <div className="col d-flex justify-content-center">
+        <div className="card border-0 text-center" style={{ width: '100%' }}>
+          <img src="images/homepage/archive.png" alt="Archive" className="card-img-top img-fluid" />
+          <div className="card-body p-2">
+            <p className="card-text small">02&nbsp;&nbsp;&nbsp;Archive</p>
+          </div>
+        </div>
+      </div>
 
+      {/* Image 5 - Studios */}
+      <div className="col d-flex justify-content-center">
+        <div className="card border-0 text-center" style={{ width: '100%' }}>
+          <img src="images/homepage/studio.png" alt="Studios" className="card-img-top img-fluid" />
+          <div className="card-body p-2">
+            <p className="card-text small">03&nbsp;&nbsp;&nbsp;Studios</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Image 6 with offset */}
+      <div className="col d-flex justify-content-center" style={{ transform: 'translateY(-140px)' }}>
+        <img src="images/homepage/hero3.png" alt="Image 6" className="img-fluid" />
+      </div>
+
+      {/* Image 7 */}
+      <div className="col d-flex justify-content-center">
+        <img src="images/homepage/hero4.png" alt="Image 7" className="img-fluid" />
+      </div>
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default Hero;
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,30 +1,83 @@
-export function Hero() {
-  
-    return (
-        <div class="row">
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/hero1.png" alt="Image 1"/>
-            </div>
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/hero2.png" alt="Image 2"/>
-            </div>
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/values_main.png" alt="Image 4"/>
-            </div>
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/archive.png" alt="Image 5"/>
-            </div>
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/studio.png" alt="Image 3"/>
-            </div>
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/hero3.png" alt="Image 6"/>
-            </div>
-            <div class="col-md-2 col-6 image-container">
-                <img src="images/homepage/hero4.png" alt="Image 7"/>
-            </div>
+import React from "react";
+
+function Hero() {
+  const imageBoxStyle = {
+    width: '140px',
+    height: '140px',
+    position: 'relative',
+  };
+
+  const imageStyle = {
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  };
+
+  const labelStyle = {
+    position: 'absolute',
+    bottom: '-24px',
+    left: '50%',
+    transform: 'translateX(-50%)',
+    fontSize: '0.8rem',
+    textAlign: 'center',
+    whiteSpace: 'nowrap',
+  };
+
+  return (
+    <div className="row justify-content-between gx-3 px-3 py-5">
+      {/* Image 1 */}
+      <div className="col d-flex justify-content-center">
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/hero1.png" alt="Image 1" style={imageStyle} />
         </div>
-    );
+      </div>
+
+      {/* Image 2 with offset */}
+      <div className="col d-flex justify-content-center" style={{ transform: 'translateY(170px)' }}>
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/hero2.png" alt="Image 2" style={imageStyle} />
+        </div>
+      </div>
+
+      {/* Image 3 - Value */}
+      <div className="col d-flex justify-content-center">
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/values.png" alt="Value" style={imageStyle} />
+          <div style={labelStyle}>01&nbsp;&nbsp;Value</div>
+        </div>
+      </div>
+
+      {/* Image 4 - Archive */}
+      <div className="col d-flex justify-content-center">
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/archive.png" alt="Archive" style={imageStyle} />
+          <div style={labelStyle}>02&nbsp;&nbsp;Archive</div>
+        </div>
+      </div>
+
+      {/* Image 5 - Studios */}
+      <div className="col d-flex justify-content-center">
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/studio.png" alt="Studios" style={imageStyle} />
+          <div style={labelStyle}>03&nbsp;&nbsp;Studios</div>
+        </div>
+      </div>
+
+      {/* Image 6 with offset */}
+      <div className="col d-flex justify-content-center" style={{ transform: 'translateY(-140px)' }}>
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/hero3.png" alt="Image 6" style={imageStyle} />
+        </div>
+      </div>
+
+      {/* Image 7 */}
+      <div className="col d-flex justify-content-center">
+        <div style={imageBoxStyle}>
+          <img src="images/homepage/hero4.png" alt="Image 7" style={imageStyle} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
