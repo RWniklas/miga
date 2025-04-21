e5f9b254ad20abd2c04bd458f1faf4f272d3df72
RWniklas <[nicki.sissigmx.de]>
2025-04-21 20:53:30 +0200

    final main

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,57 +1,59 @@
 import React from "react";
-import "./Hero.css"; // External styles
-// import '@fortawesome/fontawesome-free/css/all.min.css';
+import "./Hero.css";
 
 function Hero() {
     return (
         <div className="hero-container">
-            {/* Image 1 */}
-            <div className="image-box">
+            {/* Desktop only */}
+            <div className="image-box hide-mobile">
                 <img src="images/homepage/hero1.png" alt="Image 1" className="image" />
             </div>
 
-            {/* Image 2 with offset */}
-            <div className="image-box" style={{ transform: "translateY(160px)" }}>
+            <div className="image-box hide-mobile offset-down">
                 <i className="fa-solid fa-plus plus-icon"></i>
                 <img src="images/homepage/hero2.png" alt="Image 2" className="image" />
             </div>
 
-            {/* Image 3 - Value */}
-            <div className="border-box">
+            {/* Shared: VALUES */}
+            <div className="border-box value-box">
                 <div className="image-box">
-                    <img src="images/homepage/values.png" alt="Value" className="image" />
+                    <img src="images/homepage/values.png" alt="Values" className="image" />
                     <div className="label">
-                        <b>01&nbsp;&nbsp;Value</b>
+                        <span className="label-number">01</span>
+                        <span className="label-text">Values</span>
                     </div>
                 </div>
             </div>
 
-            {/* Image 4 - Archive */}
-            <div className="border-box">
+            {/* Shared: ARCHIVE */}
+            <div className="border-box archive-box">
                 <div className="image-box">
                     <img src="images/homepage/archive.png" alt="Archive" className="image" />
                     <div className="label">
-                        <b>02&nbsp;&nbsp;Archive</b>
+                        <span className="label-number">02</span>
+                        <span className="label-text">Archive</span>
                     </div>
                 </div>
             </div>
 
-            {/* Image 5 - Studios */}
-            <div className="border-box">
+            {/* Shared: STUDIOS */}
+            <div className="border-box studio-box">
                 <div className="image-box">
                     <img src="images/homepage/studio.png" alt="Studios" className="image" />
-                    <div className="label">03&nbsp;&nbsp;Studios</div>
+                    <div className="label">
+                        <span className="label-number">03</span>
+                        <span className="label-text">Studios</span>
+                    </div>
                 </div>
             </div>
 
-            {/* Image 6 with off-set*/}
-            <div className="image-box">
-                <i className="fa-solid fa-plus plus-icon-2"></i>
-                <img src="images/homepage/hero3.png" alt="Image 6" className="image" style={{ transform: "translateY(-170px)" }} />
+            {/* Desktop only */}
+            <div className="image-box hide-mobile offset-up">
+                <i className="fa-solid fa-plus plus-icon"></i>
+                <img src="images/homepage/hero3.png" alt="Image 6" className="image" />
             </div>
 
-            {/* Image 7 */}
-            <div className="image-box">
+            <div className="image-box hide-mobile">
                 <img src="images/homepage/hero4.png" alt="Image 7" className="image" />
             </div>
         </div>
